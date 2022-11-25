26344d865512ede2f7ba5b5517956e0f08714662
amc86 <amcuadrado1986@hotmail.com>
2022-11-25 13:34:33 +0100

    pantallas compraefectuada y comprarEntrada1 terminadas con mediaquery

--- rhitme/src/Components/BotonCierre/BotonCierre.jsx
+++ rhitme/src/Components/BotonCierre/BotonCierre.jsx
@@ -3,9 +3,12 @@ import './BotonCierre.scss';
 
 function BotonCierre() {
   return (
-    <div className='colocacioncierre' >
+    <div className='colocacioncierre'>
         <img src='/marca-x.png' className='botoncierre'></img>
+        {/* <button className='colocacioncierre' onClick={()}></button> */}
     </div>
+
+    
   )
 }
 
