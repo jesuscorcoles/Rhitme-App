138e5e7c24971307bf23344eab659a1bdd0fadef
Jesús Córcoles <jesuscorcoles@MacBook-Pro-de-Jesus.local>
2022-11-27 20:47:14 +0100

    arreglos

--- rhitme/src/Pages/Entradas/MenuEntradaPage/ConcertsAvailable/ConcertsAvailable.jsx
+++ rhitme/src/Pages/Entradas/MenuEntradaPage/ConcertsAvailable/ConcertsAvailable.jsx
@@ -2,96 +2,51 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import './ConcertsAvailable.scss';
 import { Link } from 'react-router-dom'
-
-
 export default function ConcertsAvailable () {
-
     const [singers, setSingers] = useState()
-
+    // const [salas, setSalas] = useState()
     useEffect(() => {
         const getSingers = async () => {
             const pepe = await axios.get(`http://localhost:9007/artists`);
             setSingers(pepe.data);
         }
         getSingers();
-
     }, [])
-
     return (
         <div className='genBox'>
-
-            <div>
+            <div >
                 {singers?.map((singer, index) => {
-
                     return(
                         <div className='concerts'>
                             <div className='date'>{singer.concerts.date}</div>
-                               
-                            <div className='ajusteconcerts'>
-                             <div key={index} className="eventBox">
-
+                                <div className='ajusteconcerts'>
+                                    <div key={index} className="eventBox">
                                         <div className='imgBox'>
                                             <img className='img' src={singer.image}></img>
                                         </div>
-
                                         <div className='rightblock'>
-
                                             <div className='placebox'>
                                                 <div className='place'>
                                                     {singer.halls.name}
                                                 </div>
                                             </div>
-
                                             <div className='nameAndButton'>
-
-                                                <div className='singerName'>{singer.name}
-                                                    <Link  to="/eventoseleccionado"> <button className='btn'>Info</button></Link>
+                                                <div className='singerName'>{singer.name}</div>
+                                                    <Link  to="/entradas/:artistName"> <button className='btn'>Info</button></Link>
                                                 </div>
-                                            
                                                 <div className='infoBox'>
-
                                                     <div className='genre'>
-
-                                                        <img className='miniIcons' src='musica.png'></img>
-                                                        {singer.genre.name}
-                                                        
-                                                    </div>
+                                                    <img className='miniIcons' src='musica.png'></img>{singer.genre.name}</div>
                                                     <p className='price'>{singer.concerts.price}</p>
-                                                    
                                                 </div>
-
                                             </div>
-
-                                        </div> 
-
-
-
-
-
-
-                                        <Link  to="/eventoseleccionado"> <button className='btn'>Info</button></Link>
-                                
-                                    </div>
-                                
-                                    <div className='infoBox'>
-
-                                        <div className='genre'>
-                                            <img className='miniIcons miniIcons--tickets' src='musica.png'></img>{singer.genre.name}
-                                         </div>
-                                    
-                                        <p className='price'>{singer.concerts.price}</p>
-                                    </div>       
-                                
-                            </div>
-                            
-                            <div className='line'>
-                            </div>
-
-                        </div>                                    
+                                        </div>
+                                        <div className='line'></div>
+                                    </div>  
+                                </div>
                     )
                 })}
             </div>
-                                            
         </div> 
     )
 }
