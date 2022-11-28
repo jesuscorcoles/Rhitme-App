c0cd42d251a7fc56cf954abd1ee00577dece7189
amc86 <amcuadrado1986@hotmail.com>
2022-11-28 14:53:29 +0100

    mediodía 28 alberto

--- rhitme/src/Pages/Entradas/EventoSeleccionado/EventoSeleccionado.jsx
+++ rhitme/src/Pages/Entradas/EventoSeleccionado/EventoSeleccionado.jsx
@@ -5,106 +5,63 @@ import Back from '../../../Components/Back/Back';
 import NavBar from '../../../Components/NavBar/NavBar';
 import './EventoSeleccionado.scss';
 
-
 export default function EventoSeleccionado () {
-  const [cantantes, setCantantes] = useState ({});
-  const [conciertos, setConciertos] = useState();
-  // const {artistName} = useParams();
+  const [cantantes, setCantantes] = useState ();
   const {id} = useParams();
 
-  
-
   useEffect(() => {
-    const getData = async (sing) => {
-        // const data = await axios.get(`http://localhost:9007/artists/`);
+    const getData = async () => {
         const data = await axios.get(`http://localhost:9007/artists/${id}`);
         console.log(data);
         setCantantes(data.data);
-}
+    };
   getData();
   }, []);
-
-useEffect(() => {
-    const getData = async () => {
-        const data = await axios.get(`http://localhost:9007/concerts/${id}`);
-        // const data = await axios.get(`http://localhost:9007/concerts/${artistName}`);
-        console.log(data);
-        setConciertos(data.data);
-}
-  getData();
-  }, []);
-
+  
   return (
     <div className='allContent'>
-
       <NavBar></NavBar>
-        
       <header className='header'>
           <Link to="/entradas"><Back></Back></Link>
           <p>Entradas</p>
           <div className='btnBox'></div>
       </header>
-        
-        
-
-
       <div className='eventDateImg'>
-
             <div className='singImgBox'>
-
-              <img className='singerImg' src={cantantes?.image} alt=''></img> 
+              <img className='singerImg' src={cantantes?.image} alt=''></img>
             </div>
-
             <div className='infobox'>
               <h1 className='textSing textSing--title'>{cantantes?.name}</h1>
-              <p className='textSing'>{conciertos?.date}</p>
-
+              <p className='textSing'>{cantantes?.concerts.date}</p>
                     <div className='hourAndLike'>
-
                       <div>
-
                         <div className='iconAndHour'>
                             <img className='miniIcons' src='/hora.png' alt='hora'></img>
-                            <p>{conciertos?.hour}</p>
+                            <p>{cantantes?.concerts.hour}</p>
                         </div>
-
                         <div className='iconAndHour'>
                             <img className='miniIcons' src='/euro.png' alt='precio'></img>
-                            <p>{conciertos?.price}</p>
+                            <p>{cantantes?.concerts.price}</p>
                         </div>
-                      
                       </div>
-
                         <div>
-                          <img className='like' src='corazon.png' alt='me interesa'></img>
+                          <img className='like' src='/corazon.png' alt='me interesa'></img>
                         </div>
                     </div>
             </div>
-              
-              
           </div>
-
           <div className='infoabajo'>
-
             <div className='buttonsBox'>
                 <Link to="/comprarentrada1"><button className="btnShare btnShare--buy">Comprar</button></Link>
-                
-                <Link to="" ><button className='btnShare' >Compartir</button> </Link>
+                <Link to="" ><button className='btnShare'>Compartir</button> </Link>
             </div>
-
-
             <div className='infoEvent'>
-
                   <div>
-
-                    <p><img className='miniIcons miniIcons--small' src="/location.png"></img>{conciertos?.halls.name}</p>
+                    <p><img className='miniIcons miniIcons--small' src="/location.png"></img>{cantantes?.halls.name}</p>
                     <p><img className='miniIcons miniIcons--small' src="/musica.png"></img>{cantantes?.name}</p>
-                    <p><img className='miniIcons miniIcons--small' src="/grabacion.png"></img>{conciertos?.hour} h</p>
-
+                    <p><img className='miniIcons miniIcons--small' src="/grabacion.png"></img>{cantantes?.concerts.hour} h</p>
                   </div>
             </div>
-
-
             <div className='infoConcert infoConcert--infoLarge'>
                 <p className='texts'>
                 {cantantes?.description}
@@ -112,6 +69,5 @@ useEffect(() => {
             </div>
       </div>
     </div>
-    
   )
 }
